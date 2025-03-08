14b1033f5d1d434873b95affef6556e072808766
kavin <kavin@LAPTOP-E4JGG2DN>
2025-03-08 15:12:46 +0530

    login&registration commit

---
+++ megacityCab/src/main/java/com/megacityCab/model/Rider.java
@@ -0,0 +1,21 @@
+package com.megacityCab.model;
+
+public class Rider {
+    private int riderId;
+    private int userId;
+    private String vehicleType;
+    private String vehicleModel;
+    private String vehicleNumber;
+
+   
+    public int getRiderId() { return riderId; }
+    public void setRiderId(int riderId) { this.riderId = riderId; }
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public String getVehicleType() { return vehicleType; }
+    public void setVehicleType(String vehicleType) { this.vehicleType = vehicleType; }
+    public String getVehicleModel() { return vehicleModel; }
+    public void setVehicleModel(String vehicleModel) { this.vehicleModel = vehicleModel; }
+    public String getVehicleNumber() { return vehicleNumber; }
+    public void setVehicleNumber(String vehicleNumber) { this.vehicleNumber = vehicleNumber; }
+}
